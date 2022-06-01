177d5589e645b01dc9954ce4ecb7af5dbb353c5b
Dipen Shrestha <65495407+dipenshrestha@users.noreply.github.com>
2022-06-01 09:33:42 +0545

    Add files via upload

---
+++ animeProject2022/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Anime Store</title>
+        <!--link to css-->
+		<link rel="stylesheet" type="text/css" href="styles.css">
+		<!--link to font awesome-->
+		<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+		<!--code for favicon-->
+		<link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+        <link rel="manifest" href="favicon_io/site.webmanifest">
+</head>
+<body>
+    <div id="container">
+        <div id="headContainer">
+            <div id="logohead">
+                <img src="images/fafflLogo.png" id="logoheadpic">
+            </div>
+            <nav>
+                <ul>
+                    <li class="active"><a href="index.html"><i class="fa fa-home"></i>  Home</a></li>
+                    <li><a href="#"><i class="fa fa-shopping-cart"></i>  Products</a></li>
+                    <li><a href="#"><i class="fa fa-info"></i>  About</a></li>
+                    <li><a href="#"><i class="fa fa-sign-in"></i>  Login</a></li>
+
+                    <!--code of drop down menu-->
+			        <li class="drop-menu"><a href="#"><i class="fa fa-bars"></i></a>
+                        <ul class="drop-down">
+                            <li><a href="login.html"><i class="fa fa-info"></i>  About Admin</a></li>
+                            <li><a href="#"><i class="fa fa-shopping-cart"></i>  View Items</a></li>
+                            <li><a href="#"><i class="fa fa-envelope"></i>  Admin Message</a></li>
+                            <li><a href="#"><i class="fa fa-comment"></i>  Send Message</a></li>
+                        </ul>
+			        </li>
+                </ul>
+            </nav>
+        </div>
+
+        <div id="Content">
+            helolo
+        </div>
+
+        <div id="footerr">
+            <footer>
+                    <div id="logofoot">
+                        <img src="images/fafflLogo.png" id="logofootpic">
+                    </div>
+                    <div id="copyf1">
+                        Copyright  <i class="fa fa-copyright"></i>  2022 FAFFL 
+                    </div>
+            </footer>
+        </div>
+    </div>
+</body>
+</html>
