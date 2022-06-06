047f11c20e230cde622b7d0ddf19bfeac6eb3ec5
Dipen Shrestha <65495407+dipenshrestha@users.noreply.github.com>
2022-06-07 03:17:54 +0545

    Add files via upload

--- animeProject2022/index.php
+++ animeProject2022/index.php
@@ -17,42 +17,256 @@
 </head>
 <body>
     <div id="container">
+        <!-- head and navigation menu -->
+        <section class="home" id="home">
         <div id="headContainer">
-            <div id="logohead">
-                <img src="images/fafflLogo.png" id="logoheadpic">
-            </div>
             <nav>
                 <ul>
-                    <li class="active"><a href="index.html"><i class="fa fa-home"></i>  Home</a></li>
-                    <li><a href="#"><i class="fa fa-shopping-cart"></i>  Products</a></li>
-                    <li><a href="#"><i class="fa fa-info"></i>  About</a></li>
-                    <li><a href="#"><i class="fa fa-sign-in"></i>  Login</a></li>
-
-                    <!--code of drop down menu-->
-			        <li class="drop-menu"><a href="#"><i class="fa fa-bars"></i></a>
-                        <ul class="drop-down">
-                            <li><a href="login.html"><i class="fa fa-info"></i>  About Admin</a></li>
-                            <li><a href="#"><i class="fa fa-shopping-cart"></i>  View Items</a></li>
-                            <li><a href="#"><i class="fa fa-envelope"></i>  Admin Message</a></li>
-                            <li><a href="#"><i class="fa fa-comment"></i>  Send Message</a></li>
-                        </ul>
-			        </li>
+                    <li><a href="#home"><i class="fa fa-home"></i>  Home</a></li>
+                    <li><a href="#products"><i class="fa fa-shopping-cart"></i>  Products</a></li>
+                    <li><a href="#"><i class="fa fa-shopping-cart"></i>  View Items</a></li>
+                    <li><a href="#contact"><i class="fa fa-comment"></i>  Send Message</a></li>
+                    <li><a href="#about"><i class="fa fa-info-circle"></i>  About Us</a></li>
                 </ul>
             </nav>
         </div>
 
-        <div id="Content">
-            helolo
+        <div id="content">
+            <div id="col-1">
+                    <img src="images/sololeveling1.png" id="col-1-pic">
+            </div>
+            <div class="col-2">
+                <h1>" It's foolish to fear what we have yet to see and know ! "</h1>
+                <a href="#products" class="btn">Explore Now &#8594;</a>
+            </div>
+        </div>
+        </section>
+
+        <!------ latest products ------>
+        <div class="offer">
+                    <div class="c1">
+                        <img src="images/itachi-set.png" class="offer-img1">
+                    </div>
+
+                    <div class="c2">
+                        <p>Exclusively Available on FAFFL</p>
+                        <h1>Itachi Combo</h1>
+                        <small>Itachi Combo offers 6 pieces combo accessories, which includes
+                            Anime cloak, Cosplay headband, Necklace and Ring.
+                        </small>
+                        <h2 class="price" value="59.99">Price : $59.99</h2>
+                        <a href="" class="btn">Buy Now &#8594;</a>
+                    </div>
         </div>
 
-        <div id="footerr">
+        <!------ featured products ------>
+        <section class="products" id="products">
+        <div class="small-container">
+            <h2 class="title">Featured Products</h2>
+            <div class="row">
+                <div class="col-4">
+                    <img src="images/angel-demon.jpg" width="400px">
+                    <h4>Angel Demon couple pendent</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$21.99</p>
+                    <a href="" class="btn">Buy Now &#8594;</a>
+                </div>
+                <div class="col-4">
+                    <img src="images/sololeveling-3d-lamp.jpg" width="400px">
+                    <h4>3D Lamp Anime Figure Solo Leveling</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$30.99</p>
+                    <a href="" class="btn">Buy Now &#8594;</a>
+                </div>
+                <div class="col-4">
+                    <img src="images/anya-tshirt.png" width="400px">
+                    <h4>Anya forger spyXfamily t-shirt </h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$8.99</p>
+                    <a href="" class="btn">Buy Now &#8594;</a>
+                </div>
+                <div class="col-4">
+                    <img src="images/fairy-tail.png" width="400px">
+                    <h4>Fairy Tail T-Shirt</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half-o"></i>
+                    </div>
+                    <p>$8.99</p>
+                    <a href="" class="btn">Buy Now &#8594;</a>
+                </div>
+                <div class="col-4">
+                    <img src="images/one-piece-necklace.jpg" width="400px">
+                    <h4>One Piece Necklace</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$14.99</p>
+                    <a href="" class="btn">Buy Now &#8594;</a>
+                </div>
+                <div class="col-4">
+                    <img src="images/death-note.jpg" width="400px">
+                    <h4>Death Note Bracelet</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half-o"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$9.99</p>
+                    <a href="" class="btn">Buy Now &#8594;</a>
+                </div>
+                <div class="col-4">
+                    <img src="images/ame no habakiri.jpg" width="400px">
+                    <h4>Ame no habakiri sword replica</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$45.49</p>
+                    <a href="" class="btn">Buy Now &#8594;</a>
+                </div>
+                <div class="col-4">
+                    <img src="images/enma.jpg" width="400px">
+                    <h4>Zoro sword ENMA</h4>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <p>$45.49</p>
+                    <a href="" class="btn">Buy Now &#8594;</a>
+                </div>
+            </div>
+        </div>
+        </section>
+
+        <!------ latest products ------>
+        <div class="offer">
+                    <div class="c1">
+                        <img src="images/wanted-poster.jpg" class="offer-img1">
+                    </div>
+
+                    <div class="c2">
+                        <p>Exclusively Available on FAFFL</p>
+                        <h1>One Piece All Wanted Poster</h1>
+                        <small>One Piece All Wanted Poster offers 12 wanted poster with their bounty, which includes wanted poster of Straw Hat Pirates, Kaido and Shanks.
+                        </small>
+                        <h2 class="price" value="6.99">Price : $6.99</h2>
+                        <a href="" class="btn">Buy Now &#8594;</a>
+                    </div>
+        </div>
+
+        <!-- -------about section---- -->
+        <section class="about" id="about">
+            <div class="content1">
+                <div class="title"><span style="font-size: 30px;">About us</span></div>
+                <div class="about-details">
+                    <div class="left">
+                        <p>This an online anime accessories web site. Our major commodities are Anime Merchandised Accessories and Cosplay Items which includes manga, figurines, apparel, cosplay tools, cosplay dress etc.
+                        <br><br>
+                        Our team are continuously working to fulfil the demand of our valued customer by sourcing out from any corner of the globe. We are gradually expanding our inventory every month with new and interesting merchandised products of the hottest anime series.
+                        <br><br>
+                        As the customer satisfaction is our goal, we always welcome any sorts of suggestion from anyone. Please feel free to contact for any issue that you may need. We will get back at you as soon as possible.</p>
+                        <a href="#home" class="btn">Get Started &#8594;</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- -------contact section---- -->
+        <section class="contact" id="contact">
+        <div class="contact">
+            <div class="title">
+                <span style="font-size: 30px;">Contact us</span>
+            </div>
+
+            <div class="login-form">
+                <form action="#">
+                    <label for="fname">Name</label>
+                    <input type="text" id="name" name="name" placeholder="Your name...">
+                  
+                    <label for="email">Email</label>
+                    <input type="Email" id="email" name="email" placeholder="username@email.com">
+
+                    <label for="subject">Subject</label>
+                    <input type="text" id="subject" name="subject" placeholder="your subject...">
+
+                    <label for="message">Message</label>
+                    <textarea rows="5" cols="20"></textarea>
+
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+        </section>
+
+        <!-- footer -->
+        <div id="footer">
             <footer>
+                <div class="quicknavigation">
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#products">Products</a></li>
+                        <li><a href="#">View Items</a></li>
+                        <li><a href="#contact">Send Message</a></li>
+                        <li><a href="#about">About Us</a></li>
+                    </ul>
+                </div>
+
+                <div class="row-icon">
+                    <ul class="col1-icon">
+                        <li><a href="https:\\www.facebook.com" title="facebook"   style="background-color:#3b5998">
+                        <i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https:\\www.instagram.com" title="instagram"   style="background-color:#b30086">
+                        <i class="fa fa-instagram "></i></a></li>
+                        <li><a href="https:\\www.twitter.com" title="twitter"   style="background-color:#00aced">
+                        <i class="fa fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+
+                <hr class="hrline-buttom">
+
+                <div class="copy">
                     <div id="logofoot">
                         <img src="images/fafflLogo.png" id="logofootpic">
                     </div>
-                    <div id="copyf1">
-                        Copyright  <i class="fa fa-copyright"></i>  2022 FAFFL 
+
+                    <div id="copyright">
+                        <i class="fa fa-copyright"></i> &nbsp; FAFFL - 2022 All rights reserved 
                     </div>
+                </div>
             </footer>
         </div>
     </div>
